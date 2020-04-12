abdfa23635017c8ee2b2d9f8d56a1fd1cf37cbf4
Antonio José  Sánchez <ajsamu@hotmail.com>
2020-04-12 14:12:07 +0200

    validacion completa

--- js/validacion-formulario.js
+++ js/validacion-formulario.js
@@ -80,6 +80,7 @@ window.onload = function change() {
 //      - 2 si la fecha de fin es incorrecta
 //      - 3 si ambas son incorrecta
 function validar_fechas() {
+    mostrarDetalles();
     var input1 = document.getElementById("fechaIni");
     var input2 = document.getElementById("fechaFin");
     var span1 = document.getElementById("spanFechaIni");
@@ -192,4 +193,96 @@ function validar_formulario() {
     }
 
     if (correcto) form.action = "#";
+}
+
+function mostrarDetalles() {
+    var detallesAbono = document.getElementById("detalles-abono");
+    var sel = document.getElementById("select-id");
+
+    if (sel.value == "estandar") {
+        detallesAbono.innerHTML = `
+            <h1
+            id="tipo-abono"
+            class="has-text-centered has-text-weight-bold is-size-2"
+            >
+                Abono estándar
+            </h1>
+
+            <h3 class="has-text-weight-bold is-size-4">Incluye:</h3>
+
+            <ul class="list centrar-texto">
+                <li class="list-item">
+                    Entrada al evento
+                </li>
+            </ul>
+            <h3 class="is-size-4 has-text-weight-bold centrar-texto">
+                Precio final: <b id="precio">30</b> €
+            </h3>
+        `;
+    }
+
+    if (sel.value == "medio") {
+        detallesAbono.innerHTML = `
+            <h1
+            id="tipo-abono"
+            class="has-text-centered has-text-weight-bold is-size-2"
+            >
+                Abono medio
+            </h1>
+
+            <h3 class="has-text-weight-bold is-size-4">Incluye:</h3>
+
+            <ul class="list centrar-texto">
+                <li class="list-item">
+                    Entrada al evento
+                </li>
+                <li class="list-item">
+                    Posibilidad de participar en los torneos
+                </li>
+                <li class="list-item">
+                    Premios
+                </li>
+            </ul>
+            <h3 class="is-size-4 has-text-weight-bold centrar-texto">
+                Precio final: <b id="precio">45</b> €
+            </h3>
+        `;
+    }
+
+    if (sel.value == "vip") {
+        detallesAbono.innerHTML = `
+            <h1
+            id="tipo-abono"
+            class="has-text-centered has-text-weight-bold is-size-2"
+            >
+                Abono VIP
+            </h1>
+
+            <h3 class="has-text-weight-bold is-size-4">Incluye:</h3>
+
+            <ul class="list centrar-texto">
+                <li class="list-item">
+                    Entrada al evento
+                </li>
+                <li class="list-item">
+                    Posibilidad de participar en los torneos
+                </li>
+                <li class="list-item">
+                    Premios
+                </li>
+                <li class="list-item">
+                    Entrada a los lugares donde se encuentran los invitados
+                </li>
+                <li class="list-item">
+                    Alojamiento en el Hotel Playa Victoria
+                </li>
+                <li class="list-item">
+                    Desplazamiento desde hotel a evento
+                </li>
+            </ul>
+            <h3 class="is-size-4 has-text-weight-bold centrar-texto">
+                Precio final: <b id="precio">100</b> €
+            </h3>
+        `;
+    }
 }
